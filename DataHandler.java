c654322c09dcd411b0c47618968c37b0b58efd62
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-19 14:21:23 -0900

    The exception handler was included

--- DataHandler.java
+++ DataHandler.java
@@ -31,59 +31,61 @@ public class DataHandler {
      */
     private void populateList() {
 
-        //Create a file that will be used to populate list
-        File inFile = new File("Lineages.txt");
-
-        //Check if the file exists before working with it
-        if (inFile.exists()) {
-            //Declare this to read the contents f the file
-            Scanner input = new Scanner(inFile);
-            String inputLine;
-            Person p = new Person();
-            ArrayList<String> children = new ArrayList<>();//The children names are stored here
+        try {
+            //Create a file that will be used to populate list
+            File inFile = new File("Lineages.txt");
+            //Check if the file exists before working with it
+            if (inFile.exists()) {
+                //Declare this to read the contents f the file
+                Scanner input = new Scanner(inFile);
+                String inputLine;
+                Person p = new Person();
+                ArrayList<String> children = new ArrayList<>();//The children names are stored here
 
             /*Extracts a single line of input from file
             while there is still input to read
              */
-            while (input.hasNext()) {
+                while (input.hasNext()) {
 
-                //Get the line of input string
-                inputLine = input.nextLine();
-                //Split  the names from input into an array
-                String [] names = inputLine.split(",");
+                    //Get the line of input string
+                    inputLine = input.nextLine();
+                    //Split  the names from input into an array
+                    String[] names = inputLine.split(",");
 
-                //Build a Person object from input and add to lineageList
-                for(int i = 0;i < names.length;i ++){
+                    //Build a Person object from input and add to lineageList
+                    for (int i = 0; i < names.length; i++) {
 
-                    if(i == 0){
-                        //Person's name
-                        p.setName(names[i]);
-                    }else if(i == 1){
-                        //Mom name
-                        p.setMomName(names[i]);
-                    }else if(i == 2){
-                        //Dad's name
-                        p.setDadName(names[i]);
-                    }else{
-                        //Add to local ArrayList
-                        children.add(names[i]);
+                        if (i == 0) {
+                            //Person's name
+                            p.setName(names[i]);
+                        } else if (i == 1) {
+                            //Mom name
+                            p.setMomName(names[i]);
+                        } else if (i == 2) {
+                            //Dad's name
+                            p.setDadName(names[i]);
+                        } else {
+                            //Add to local ArrayList
+                            children.add(names[i]);
 
+                        }
                     }
+
+                    //Set the Person object's children
+                    p.setChildrenNames(children);
+
+                    //Add the newly constructed Person to lineageList
+                    lineageList.add(p);
+
                 }
 
-                //Set the Person object's children
-                p.setChildrenNames(children);
-
-                //Add the newly constructed Person to lineageList
-                lineageList.add(p);
 
             }
-
-
-        }else{
-              //Figure the Exception handling and continue with work
+        }catch(Exception e){
 
         }
     }
+
+
 
 }
