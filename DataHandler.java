62d3461b9958a6c826fbd235dba2846e86bc7bee
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-19 16:11:56 -0900

    DataHandler is now a Singleton

--- DataHandler.java
+++ DataHandler.java
@@ -11,20 +11,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/*The class should only be created once because it uses a lot of data and time
+so we use the Singleton design patter to achieve our goal
+ */
+
 public class DataHandler {
 
+    //An instance of object for Singleton
+    private static DataHandler obj;
     //The object has one member, an ArrayList of Person objects
     private ArrayList<Person> lineageList = new ArrayList<>();
     private int indexOfName; //Stores an index pointing to a person in the list
 
 
     /* Constructor takes no arguments and calls
-    populate list  to get data into lineage lis
+    populate list  to get data into lineage list.
+    This constructor is private because we don't want more than one instance
      */
-    public DataHandler() {
+    private DataHandler() {
 
         //Call it's method that populates the lineageList field
         populateList();
+    }
+
+    //Factory method to create it's own instaance
+    public static DataHandler getInstance(){
+
+        //check if instance has not been created
+        if(obj == null){
+
+            obj = new DataHandler();
+        }
+
+        return obj;
     }
 
     /*Populates the lineageList and stores information
