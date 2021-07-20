0b4f0714a02e57a10baf8509f8d5f1c071560d88
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-19 17:35:08 -0900

    Added the main class

---
+++ LineageMain.java
@@ -0,0 +1,12 @@
+package Lineage;
+/*User is prompted to input a Name
+The program searches for it in it's data and returns
+The person's name, mom's name, father's name and children
+Else it prints that person is not in the base
+ */
+public class LineageMain {
+
+    public static void main(String[]args){
+
+    }
+}
