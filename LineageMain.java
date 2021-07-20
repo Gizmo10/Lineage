6acbd2c7026f6bd5b5cdd3a0ff10d7be10b8503b
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-19 17:46:54 -0900

    The main class is added, but will be modified with regular expressions

--- LineageMain.java
+++ LineageMain.java
@@ -1,4 +1,7 @@
 package Lineage;
+
+import java.util.Scanner;
+
 /*User is prompted to input a Name
 The program searches for it in it's data and returns
 The person's name, mom's name, father's name and children
@@ -8,5 +11,16 @@ public class LineageMain {
 
     public static void main(String[]args){
 
+        Scanner in = new Scanner(System.in);
+        DataHandler data = DataHandler.getInstance();
+        /*The user is prompted to input the name of person
+        whose information they would like to display
+         */
+        System.out.println("Please Enter a person's name?");
+        //Regular expression to be used after testing program with file
+        String name = in.nextLine();
+
+        //The name is looked up
+        data.getPersonInfo(name);
     }
 }
